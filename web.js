df07728dd5e387f416b5923fbb84f51cca4e459a
Jason Chen <nycmits@gmail.com>
2013-07-04 23:57:32 +0000

    fixed fs

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require("fs")
 
 app.get('/', function(request, response) {
-  response.send(fd.readFileSync('index.html'));
+  response.send(fs.readFileSync('index.html'));
 //  response.send('Hello World 2!');
 });
 
