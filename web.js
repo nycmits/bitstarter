264724aace16c3c1877c72f4b20b21aeeacdf1c5
Jason Chen <nycmits@gmail.com>
2013-07-04 23:53:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require("fs")
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fd.readFileSync('index.html'));
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
